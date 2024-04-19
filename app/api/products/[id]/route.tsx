5702581112eadee8d3675cc9d91ad4e16f5efc14
FENG <982685807@qq.com>
2024-04-19 18:06:56 +0800

    product database practice

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -2,22 +2,44 @@ import { NextRequest,NextResponse } from "next/server";
 import schema from "../schema";
 import prisma from "@/prisma/client";
 
-export async function PUT(request:NextRequest,{params}:{params:{id:number}}){
+export async function PUT(request:NextRequest,{params}:{params:{id:string}}){
     //validation
     const body = await request.json();
     const validation = schema.safeParse(body);
     if(!validation.success){
         return NextResponse.json(validation.error.errors,{status:404})
     }
-    if(params.id>10){
+    const product = await prisma.product.findUnique({
+        where:{
+            id: parseInt(params.id)
+        }
+    })
+    if(!product){
         return NextResponse.json({error:'products not exist'},{status:400})
     }
-    return NextResponse.json({name:body.name,price:body.price},{status:200});
+    const newProduct = await prisma.product.update({
+        where:{id:product.id},
+        data:{
+            name:body.name,
+            price:body.price
+        }
+    })
+    return NextResponse.json(newProduct,{status:200});
 }
 
-export function DELETE(request:NextRequest,{params}:{params:{id:number}}){
-    if(params.id>10){
+export async function DELETE(request:NextRequest,{params}:{params:{id:string}}){
+    const product = await prisma.product.findUnique({
+        where:{
+            id: parseInt(params.id)
+        }
+    })
+    if(!product){
         return NextResponse.json({error:'products not exist'},{status:400});
     }
-    return NextResponse.json({},{status:200});
+    await prisma.product.delete({
+        where:{
+            id: product.id
+        }
+    })
+    return NextResponse.json({});
 }
